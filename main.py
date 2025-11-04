14a66745b5adf6c7347f5f30b50c6bea15cd8ea3
Veyn <152045548+Specptr@users.noreply.github.com>
2025-11-04 20:42:00 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+
+# -----------------------------------------------
+# AetherTrace: Nomi
+# Created with curiosity and caffeine by Specptr
+# If you're reading this, you're probably awesome.
+# -----------------------------------------------
+
+from aethertrace import AetherTracePoint
+
+if __name__ == "__main__":
+    app = AetherTracePoint()
+    app.run()
